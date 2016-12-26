1c37b572dcbc3e1eb36836b816af6abb9de8f5f0
Lazaros Kazantzis <lakazantzis@gmail.com>
2016-12-26 20:33:52 +0200

    Reset greek locale as gr.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Language.create([{id: 1, locale: 'en', language: 'english'}, {id: 2, locale: 'el', language: 'greek'}])
+Language.create([{id: 1, locale: 'en', language: 'english'}, {id: 2, locale: 'gr', language: 'greek'}])
 
 Size.create(label: 'Default', ordering: 1)
 Translation.create(table_name: Size.table_name, locale: Language.find(2).locale, reference_id: Size.find(1).id, fields: {label: 'Κανονικό'})
