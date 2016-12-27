6c9fbc9d9ed02e1b4077f16ebea4c79c465a51cb
Lazaros Kazantzis <lakazantzis@gmail.com>
2016-12-27 15:01:29 +0200

    Prevent default size from being deleted.

--- app/models/size.rb
+++ app/models/size.rb
@@ -9,4 +9,18 @@ class Size < ApplicationRecord
   
   # Set default pagination values.
   include Pageable
+
+  before_destroy :check_for_default_size, prepend: true
+
+  private
+
+  def check_for_default_size
+    if self.id == 1
+      # For Rails <= 4 use:
+      # return false
+
+      # For Rails 5 use:
+      throw(:abort)
+    end
+  end
 end
