e5dc52efcf5553f4c371c07c66b4b664e6174cef
Lazaros Kazantzis <lakazantzis@gmail.com>
2017-01-09 23:12:45 +0200

    Fixed order creation bug.

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,7 +1,7 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
-  belongs_to :product, optional: true
-  belongs_to :size, optional: true
+  belongs_to :product
+  belongs_to :size
 
-  # serialize :metadata, JSON
+  serialize :metadata, JSON
 end
