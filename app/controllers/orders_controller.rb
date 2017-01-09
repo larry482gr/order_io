f7fd8ae0bc52c8e4731dc5af09f8dac5eb53239d
Lazaros Kazantzis <lakazantzis@gmail.com>
2017-01-09 22:41:03 +0200

    Fixed order creation bug.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,6 +26,11 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    unless order_params[:order][:order_products_attributes].nil?
+      order_params[:order][:order_products_attributes].each do |pr_attr|
+        @order.order_products.build pr_attr
+      end
+    end
 
     respond_to do |format|
       if @order.save
