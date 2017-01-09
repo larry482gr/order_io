b9abf06af69cca9798addd299c5c496231eca2aa
Lazaros Kazantzis <lakazantzis@gmail.com>
2017-01-09 20:36:47 +0200

    Added Orders to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
     resources :languages, only: [:index], defaults: { format: :json }
     resources :product_sizes, only: [:index], defaults: { format: :json }
-    # resources :orders
+    resources :orders
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'visitors#index'
   end
