3b4bc58e41a4490d8803710c5a062910af0de2eb
Lazaros Kazantzis <lakazantzis@gmail.com>
2017-01-09 21:29:08 +0200

    Removed authorization process from order creation.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,11 @@ Rails.application.routes.draw do
 
     resources :languages, only: [:index], defaults: { format: :json }
     resources :product_sizes, only: [:index], defaults: { format: :json }
-    resources :orders
+    resources :orders do
+      collection do
+        post :create_from_mobile, defaults: { format: :json }
+      end
+    end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'visitors#index'
   end
