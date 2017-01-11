4403b0a680ba3834c8f3318687bd09f3ccbfdc9b
Lazaros Kazantzis <lakazantzis@gmail.com>
2017-01-11 17:12:33 +0200

    Fixed order process bug.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :table
-  has_many :order_products
+  has_many :order_products, inverse_of: :order
   has_many :products, :through => :order_products
 
   accepts_nested_attributes_for :order_products
